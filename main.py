e03af009671dd9dee2f82fef4fa94f20100f041b
jurajpaska8 <jurajpaska8@gmail.com>
2020-10-31 00:59:22 +0100

    svm done, needed to do overfit network

--- main.py
+++ main.py
@@ -7,10 +7,15 @@
 import pandas as pd
 import keras as keras
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
+from sklearn.pipeline import make_pipeline
 
 from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.svm import SVC
 
 
 def load_data(filepath):
@@ -57,53 +62,59 @@ if __name__ == '__main__':
     X_test = test_data_normalized.drop(['playlist_genre'], axis=1)
     Y_test = test_data_normalized.playlist_genre
 
-    # Create the model
-    model = Sequential()
-    model.add(Dense(12, input_dim=12, activation='sigmoid'))
-    model.add(Dense(12, activation='sigmoid'))
-    model.add(Dense(6, activation='softmax')) #sigmooid  ?
-
-    # Configure the model and start training
-    optimizer = keras.optimizers.Adam(lr=0.001)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
-    history = model.fit(X_train.to_numpy(), pd.get_dummies(Y_train), epochs=500, batch_size=300, verbose=1, validation_split=0.15, callbacks=[early_stopping])
-
-    # Test the model after training
-    test_results = model.evaluate(X_test.to_numpy(), pd.get_dummies(Y_test), verbose=1)
-    pred_Y = model.predict(X_test.to_numpy())
-    pred_Y_classes = model.predict_classes(X_test.to_numpy())
-    print(f'Test results - Loss: {test_results[0]} - Accuracy: {test_results[1]}%')
-
-    # Plot training process
-    # Accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.show()
-    # Loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.show()
-
-    # # accuracy on test data
-    # predict_test = mlp.predict(test_data_normalized.drop(['playlist_genre'], axis=1))
-    # cm_test = confusion_matrix(test_data_normalized.playlist_genre, predict_test)
-    # acc_test = accuracy_score(test_data_normalized.playlist_genre, predict_test)
+    # # Create the model
+    # model = Sequential()
+    # model.add(Dense(12, input_dim=12, activation='sigmoid'))
+    # model.add(Dense(12, activation='sigmoid'))
+    # model.add(Dense(6, activation='softmax')) #sigmoid  ?
     #
-    # # accuracy on train data
-    # predict_train = mlp.predict(x)
-    # cm_train = confusion_matrix(y, predict_train)
-    # acc_train = accuracy_score(y, predict_train)
+    # # Configure the model and start training
+    # optimizer = keras.optimizers.Adam(lr=0.001)
+    # model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    # early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+    # history = model.fit(X_train.to_numpy(), pd.get_dummies(Y_train), epochs=500, batch_size=300, verbose=1, validation_split=0.15, callbacks=[early_stopping])
+    #
+    # # Test the model after training
+    # test_results = model.evaluate(X_test.to_numpy(), pd.get_dummies(Y_test), verbose=1)
+    # pred_Y = model.predict(X_test.to_numpy())
+    # pred_Y_classes = model.predict_classes(X_test.to_numpy())
+    # print(f'Test results - Loss: {test_results[0]} - Accuracy: {test_results[1]}%')
+    #
+    # # Plot training process
+    # # Accuracy
+    # plt.plot(history.history['accuracy'])
+    # plt.plot(history.history['val_accuracy'])
+    # plt.title('model accuracy')
+    # plt.ylabel('accuracy')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'val'], loc='upper left')
+    # plt.show()
+    # # Loss
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['val_loss'])
+    # plt.title('model loss')
+    # plt.ylabel('loss')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'val'], loc='upper left')
+    # plt.show()
+    #
+    # # accuracy on test data
+    # cm_test = confusion_matrix(Y_test, pred_Y_classes)
+    # acc_test = accuracy_score(Y_test, pred_Y_classes)
 
     # SVM
+    c_range = np.logspace(-2, 5, 5)
+    gamma_range = np.logspace(-5, 3, 5)
+    params = dict(gamma=gamma_range, C=c_range)
 
+    cv = StratifiedShuffleSplit(n_splits=2, test_size=0.2, random_state=42)
+    grid = GridSearchCV(SVC(verbose=1), param_grid=params, cv=cv, verbose=1)
+    grid.fit(X_train, Y_train)
+    scores = grid.cv_results_['mean_test_score'].reshape(len(c_range), len(gamma_range))
 
+    # simple SVM
+    clf = make_pipeline(StandardScaler(), SVC(gamma='auto', verbose=1))
+    clf.fit(X_train, Y_train)
+    svm_score_train = clf.score(X_train, Y_train)
+    svm_score_test = clf.score(X_test, Y_test)
     end = 1
