a7aa9892fa0eab84f4e9568565d05bcbea32cee4
jurajpaska8 <jurajpaska8@gmail.com>
2020-11-26 23:44:39 +0100

    eda done

--- SUNS4.py
+++ SUNS4.py
@@ -1,5 +1,7 @@
 import funs
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
     ########################################################################
@@ -16,31 +18,140 @@ if __name__ == '__main__':
 
     # EDA - three questions
     # multiplayer more expensive, played, ranked in general
-    # find multiplayers and export it
-    # stream_EDA = stream_data.copy(deep=True)
-    # stream_EDA['multiplayer'] = 0
-    #
-    # multiplayer_cols = [col for col in streamspy_data.columns if 'multiplayer' in col]
-    # idx = 0
-    # for id in stream_EDA.appid:
-    #     row_num = streamspy_data.index[streamspy_data['appid'] == id].tolist()[0]
-    #     i = 0
-    #     for col in multiplayer_cols:
-    #         col_num = streamspy_data.columns.get_loc(col)
-    #         i += streamspy_data.iat[row_num, col_num]
-    #     if i > 0:
-    #         stream_EDA.at[idx, 'multiplayer'] = 1
-    #     idx += 1
-    # stream_EDA.to_csv('./data4/multiplayer.csv')
+    # find multiplayers and exports it
+    # funs.export_multiplayer_games(stream_data, streamspy_data, './data4/multiplayer.csv')
+    steam_EDA = funs.load_data('./data4/multiplayer.csv') # TODO find out why is added first column
+    multiplayer_games = steam_EDA.loc[steam_EDA['multiplayer'] == 1]
+    singleplayer_games = steam_EDA.loc[steam_EDA['multiplayer'] == 0]
 
-    steam_EDA = funs.load_data('./data4/multiplayer.csv')
+    singleplayer_played_hours_avg = singleplayer_games["average_playtime"].mean()/100
+    multiplayer_played_hours_avg = multiplayer_games["average_playtime"].mean()/100
 
+    singleplayer_price_avg = singleplayer_games["price"].mean()
+    multiplayer_price_avg = multiplayer_games["price"].mean()
+
+    singleplayer_feedbacks_avg = singleplayer_games["positive_ratings"].mean()/1000
+    multiplayer_feedbacks_avg = multiplayer_games["positive_ratings"].mean()/1000
+
+    # utilities : average prize / average playtime / average positive:negative feedbacks
+    labels = ['price', 'hours/100', 'feedbacks/1000']
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2, [singleplayer_price_avg, singleplayer_played_hours_avg, singleplayer_feedbacks_avg], width, label='SinglePlayer')
+    rects2 = ax.bar(x + width / 2, [multiplayer_price_avg, multiplayer_played_hours_avg, multiplayer_feedbacks_avg], width, label='MultiPlayer')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by feature')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    plt.show()
 
     # cheaper games more answered
-    # what features contain 'classic' games
+    steam_EDA['all_ratings'] = steam_EDA['positive_ratings'] + steam_EDA['negative_ratings']
+    # split into 11 categories according to prize
+    intervals = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, steam_EDA['price'].max() + 1]
+    all_ratings_avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    positive_ratings_avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    relative_ratings_avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(len(intervals) - 1):
+        tmp = steam_EDA[(intervals[i] <= steam_EDA['price']) & (steam_EDA['price'] < intervals[i + 1])]
+        all_ratings_avg[i] = tmp['all_ratings'].mean()
+        positive_ratings_avg[i] = tmp['positive_ratings'].mean()
+        relative_ratings_avg[i] = (tmp['positive_ratings'] / tmp['all_ratings']).mean()
+
+    labels = ['0-5', '5-10', '10-15', '15-20', '20-25', '25-30', '30-35', '35-40', '40-45', '45-50', '50+']
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # t
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2, all_ratings_avg,
+                    width, label='All ratings')
+    rects2 = ax.bar(x + width / 2, positive_ratings_avg,
+                    width, label='Positive')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Ratings')
+    ax.set_title('Ratings by price group')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    plt.show()
+
+    # relative ratings
+    fig, ax = plt.subplots()
+
+    # Using set_dashes() to modify dashing of an existing line
+    line1, = ax.plot(labels, relative_ratings_avg, label='Relative ratings)')
+    #line1.set_dashes([2, 2, 10, 2])  # 2pt line, 2pt break, 10pt line, 2pt break
+
+    ax.legend()
+    plt.show()
+
+    # what features contain 'classic' games TODO
 
     # ENCODE developer, publisher, genre
+    # funs.publishers_developers_encoded(stream_data, './data4/steamPubDevEncoded.csv')
+    encoded = funs.load_data('./data4/steamPubDevEncoded.csv')
+    encoded['Multiplayer'] = 0
+    encoded['Singleplayer'] = 0
+    encoded['Action'] = 0
+    encoded['Adventure'] = 0
+    encoded['Indie'] = 0
+    encoded['Strategy'] = 0
+    encoded['RPG'] = 0
+    encoded['Casual'] = 0
+    encoded['Simulation'] = 0
+    encoded['Racing'] = 0
+    encoded['Sports'] = 0
 
+    encoded['2D'] = 0
+    encoded['3D'] = 0
+    encoded['old_school'] = 0
+    encoded['classic'] = 0
+    encoded['war'] = 0
+    encoded['e_sports'] = 0
+    encoded['team_based'] = 0
+
+    idx = 0
+    for index in encoded.appid:
+        if 'Single-player' in encoded.at[idx, 'categories']:
+            encoded.at[idx, 'Singleplayer'] = 1
+        if 'Multi-player' in encoded.at[idx, 'categories']:
+            encoded.at[idx, 'Multiplayer'] = 1
+        if 'Action' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Action'] = 1
+        if 'Adventure' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Adventure'] = 1
+        if 'Indie' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Indie'] = 1
+        if 'Strategy' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Strategy'] = 1
+        if 'RPG' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'RPG'] = 1
+        if 'Casual' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Casual'] = 1
+        if 'Simulation' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Simulation'] = 1
+        if 'Racing' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Racing'] = 1
+        if 'Sports' in encoded.at[idx, 'genres']:
+            encoded.at[idx, 'Sports'] = 1
+        idx += 1
+
+    cols_2d = [col for col in streamspy_data.columns if '2d' in col]
+    idx = 0
+    for id in encoded.appid:
+        row_num = streamspy_data.index[streamspy_data['appid'] == id].tolist()[0]
+        i = 0
+        for col in cols_2d:
+            col_num = streamspy_data.columns.get_loc(col)
+            i += streamspy_data.iat[row_num, col_num]
+        if i > 0:
+            encoded.at[idx, '2D'] = 1
+        idx += 1
     # NORMALIZE
 
     ########################################################################
--- funs.py
+++ funs.py
@@ -9,5 +9,64 @@ def drop_all_na(dataframe):
     return pd.DataFrame.dropna(dataframe)
 
 
-def export_multiplayer_games(path):
-    return 0
+# returns DF with multiplayer value
+def export_multiplayer_games(steam_data, steamspy_data, path):
+    steam_EDA = steam_data.copy(deep=True)
+    steam_EDA['multiplayer'] = 0
+
+    multiplayer_cols = [col for col in steamspy_data.columns if 'multiplayer' in col]
+    idx = 0
+    for id in steam_EDA.appid:
+        row_num = steamspy_data.index[steamspy_data['appid'] == id].tolist()[0]
+        i = 0
+        for col in multiplayer_cols:
+            col_num = steamspy_data.columns.get_loc(col)
+            i += steamspy_data.iat[row_num, col_num]
+        if i > 0:
+            steam_EDA.at[idx, 'multiplayer'] = 1
+        idx += 1
+    steam_EDA.set_index('appid')
+    steam_EDA.to_csv(path)
+    return steam_EDA
+
+
+def publishers_developers_encoded(steam_data, path):
+    publishers = steam_data.publisher.value_counts()
+    for p, count in publishers.items():
+        steam_data['publisher'] = steam_data['publisher'].replace([p], count)  # good
+
+    developers = steam_data.developer.value_counts()
+    for d, count in developers.items():
+        steam_data['developer'] = steam_data['developer'].replace([d], count)  # good
+
+    steam_data.to_csv(path)
+
+
+def category_encoded(steam_data):
+    steam_data = pd.DataFrame
+    steam_data['Multiplayer'] = 0
+    steam_data['Singleplayer'] = 0
+
+    steam_data['Action'] = 0
+    steam_data['Adventure'] = 0
+    steam_data['Indie'] = 0
+    steam_data['Strategy'] = 0
+    steam_data['RPG'] = 0
+    steam_data['Casual'] = 0
+    steam_data['Simulation'] = 0
+    steam_data['Racing'] = 0
+    steam_data['Sports'] = 0
+
+    steam_data['2D'] = 0
+    steam_data['3D'] = 0
+    steam_data['old_school'] = 0
+    steam_data['classic'] = 0
+    steam_data['war'] = 0
+    steam_data['e_sports'] = 0
+    steam_data['team_based'] = 0
+
+    for index, row in steam_data:
+        if 'Single-player' in row['categories']:
+            row['Singleplayer'] = 1
+        if 'Multi-player' in row['categories']:
+            row['Multiplayer'] = 1
